0e689a7576b3060f8549c0e40c38793e289046ac
vishnureddy6301 <Vishnureddy6301@gmail.com>
2024-05-07 16:25:12 +0530

    Second  Commit

--- src/Components/Students.jsx
+++ src/Components/Students.jsx
@@ -16,16 +16,27 @@ const Students = () => {
  const studentsWithHighMarksAndGradeA = studentsData.filter(
     (student) => student.marks > 90 && student.grade === "A"
   );
+  console.log("students with marks greater than 90 and having an A grade",studentsWithHighMarksAndGradeA );
   const studentsWithLowMarksAndGradeF = studentsData.filter(
     (student) => student.marks < 50 && student.grade === "F"
   );
+  console.log("Students with marks less than 50 and having an F grade:", studentsWithLowMarksAndGradeF);
+
   const averageStudents = studentsData.filter(
     (student) => student.marks >= 50 && student.marks <= 90 && student.grade === "B"
   );
+  console.log("Average students with marks between 50 to 90 and having a B grade:", averageStudents);
+
   const totalMarks = studentsData.reduce((emp, student) => emp + student.marks, 0);
+  console.log("Total sum of all students' marks:", totalMarks);
+
   const sortedStudents = studentsData.sort((a, b) => a.marks - b.marks);
-  //const sortedStudents = studentsData.sort((a, b) => b.marks - a.marks);
+  console.log("Students list sorted in ascending order by marks:", sortedStudents);
+
+
+
   const studentsWithoutFGrade = studentsData.filter((student) => student.grade !== "F");
+  console.log("Students list without F grade:", studentsWithoutFGrade);
 
 
   return (
